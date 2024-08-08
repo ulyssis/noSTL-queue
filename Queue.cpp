540532f14f8e233a88a624020f2baa8615968e04
ulyssis <li.di.china@gmail.com>
2024-08-08 23:03:10 +0200

    add test

--- Queue.cpp
+++ Queue.cpp
@@ -1,53 +1,5 @@
 #include <iostream>
-
-// Node has integer value
-class Node
-{
-    int val;
-    Node *next;
-
-public:
-    Node(int value) : val(value), next(nullptr) {}
-    void copyNode(Node *node)
-    {
-        val = node->val;
-        next = node->next;
-    }
-    Node *getNext() const
-    {
-        return next;
-    }
-    void setNext(Node *node)
-    {
-        next = node;
-    }
-    void setVal(int value)
-    {
-        val = value;
-    }
-    int getVal()
-    {
-        return val;
-    }
-};
-
-// This queue consists of node with integer values
-class Queue
-{
-    int capacity;
-    Node *head;
-    Node *tail;
-    int size;
-
-public:
-    Queue(int capacity) : capacity(capacity), head(nullptr), tail(nullptr), size(0) {}
-    void push_back(Node *node); // add a new node at the end
-    Node *pop();                // retrive the head node
-    void clear();               // clear the queue
-    int getSize() const;        // return the number of nodes in the queue
-    int getCapacity() const;    // capacity
-    void printAllValues() const;
-};
+#include "Queue.h"
 
 void Queue::printAllValues() const
 {
@@ -119,34 +71,34 @@ void Queue::clear()
     tail = nullptr;
 }
 
-int main()
-{
-    Node *a = new Node(5);
-    Node *b = new Node(6);
-    Node *c = new Node(7);
-    Queue *q = new Queue(10);
-    std::cout << "the capacity of queue is: " << q->getCapacity() << "\n"
-              << "size is: " << q->getSize() << std::endl;
+// int main()
+// {
+//     Node *a = new Node(5);
+//     Node *b = new Node(6);
+//     Node *c = new Node(7);
+//     Queue *q = new Queue(10);
+//     std::cout << "the capacity of queue is: " << q->getCapacity() << "\n"
+//               << "size is: " << q->getSize() << std::endl;
 
-    std::cout << "// insert a node " << std::endl;
-    q->push_back(a);
-    std::cout << "the capacity of queue is: " << q->getCapacity() << "\n"
-              << "size is: " << q->getSize() << std::endl;
+//     std::cout << "// insert a node " << std::endl;
+//     q->push_back(a);
+//     std::cout << "the capacity of queue is: " << q->getCapacity() << "\n"
+//               << "size is: " << q->getSize() << std::endl;
 
-    std::cout << "// insert another  node " << std::endl;
-    q->push_back(b);
-    std::cout << "the capacity of queue is: " << q->getCapacity() << "\n"
-              << "size is: " << q->getSize() << std::endl;
+//     std::cout << "// insert another  node " << std::endl;
+//     q->push_back(b);
+//     std::cout << "the capacity of queue is: " << q->getCapacity() << "\n"
+//               << "size is: " << q->getSize() << std::endl;
 
-    std::cout << "// insert and then popup a node " << std::endl;
-    q->push_back(c);
-    q->printAllValues();
-    Node *temp = q->pop();
-    // std::cout << "the value of the poped up node is: " << temp->getVal() << std::endl;
-    // q->printAllValues();
-    delete a;
-    delete b;
-    delete c;
-    delete q;
-    return 0;
-}
+//     std::cout << "// insert and then popup a node " << std::endl;
+//     q->push_back(c);
+//     q->printAllValues();
+//     Node *temp = q->pop();
+//     std::cout << "the value of the poped up node is: " << temp->getVal() << std::endl;
+//     q->printAllValues();
+//     delete a;
+//     delete b;
+//     delete c;
+//     delete q;
+//     return 0;
+// }
